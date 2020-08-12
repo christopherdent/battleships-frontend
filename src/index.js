2b253c62d2e62f91efcff2143e99ad918105325e
Christopher Dent <christopherdent01@gmail.com>
2020-08-11 21:22:37 -0400

    various fixes in line with backend commits

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
-const BASE_URL = "http://localhost:3000"
-const TRAINERS_URL = `${BASE_URL}/users`
-const POKEMONS_URL = `${BASE_URL}/ships`
-const main = document.getElementById("main")
-
-// var person = prompt("Please enter your name", "Harry Potter");
-  // if (person == null || person == "") {
-  //   txt = "User cancelled the prompt.";
-  // } else {
-  //
-  //   txt = "Hello " + person + "! How are you today?";
-  // }
+const endPoint = "http://localhost:3000/ships"
 
 function getShips(){
-  return fetch("http://localhost:3000/ships")
+  return fetch(endPoint)
     .then(function(response) {
       return response.json();
     })
@@ -20,7 +9,9 @@ function getShips(){
       json.forEach(ship => console.log(ship.name));
       json.forEach(ship => buildShipCard(ship))
     });
-}
+    greyShipCard()
+  }
+
 function buildShipCard(ship) {
   const card = document.createElement('DIV')
     card.setAttribute("class", "card")
@@ -29,47 +20,71 @@ function buildShipCard(ship) {
     const para2 = document.createElement('P')
     const btn = document.createElement("BUTTON")
     btn.setAttribute('data-id', ship.id)
-    // const ul = document.createElement("UL")
-    // const li = document.createElement("LI")
-    // const li2 = document.createElement("LI")
-    // const li3 = document.createElement("LI")
+    btn.setAttribute('class', 'addBtn' )
+
     let img = document.createElement("img");
       img.src = ship.image
       para.innerText = ship.name
       para2.innerText = `${ship.country} / ${ship.kind}`
-      // li2.innerText = ship.kind
-      // li3.innerText = ship.image
+
       img.width = "150";
       img.height = "150";
       para.innerText = ship.name
       btn.innerText = "Add to Fleet"
-    // btn.addEventListener("click", createPokemon)
+      btn.addEventListener("click", greyShipCard)
+
     document.getElementById('main').appendChild(card)
       card.appendChild(img)
       card.appendChild(para)
       card.appendChild(para2)
       card.appendChild(btn)
-        // ship.rainer.pokemons.forEach(pokemon => renderPokemon(pokemon))
-        // ul.appendChild(li)
-        // ul.appendChild(li2)
-
 }
-getShips()
-// function renderShip(ship){
-//   const ul = document.querySelector(`div[data-id="${pokemon.trainer_id}"]`)
-//   const li = document.createElement("LI")
-//   const btn = document.createElement("button")
+
+function greyShipCard(event){
+    event.preventDefault()
+      // let divs = Array.from(document.querySelectorAll('[data-id]'));
+      // let btns = Array.from(document.getElementsByClassName("addBtn"))
+      //You'll have to add logic here to physically move the boat into a Fleet object as soon as it disappers from "Avail ships"
+
+      const configObj = {
+          method: "POST",
+          headers: {"Conent-Type": "application/json"},
+          
+          body: JSON.stringify({ ship_id: event.target.dataset.id })
+        }
+
+      fetch("http://localhost:3000/fleets", configObj)
+        .then(response => response.json())
+        .then(fleet => console.log(fleet));
+
+      console.log(event)
+        event.target.parentElement.remove()
+  }
+
+
+// function postShips(event){
+//   event.preventDefault()
 //
-//   li.innerText = `${pokemon.nickname} (${pokemon.species})`
-//   btn.setAttribute("class", "release")
-//   btn.innerText = "Release"
-//   btn.addEventListener("click", deletePokemon)
-//   btn.setAttribute("data-pokemon-id", pokemon.id)
-//   li.appendChild(btn)
-//   ul.appendChild(li)
+//   const configObj = {
+//       method: "POST",
+//       headers: {"Conent-Type": "application/json"},
+//       body: JSON.stringify({ ship_id: event.target.dataset.id })
+//     }
 //
+//   fetch("http://localhost:3000/ships", configObj)
+//     .then(response => response.json())
+//     .then(ship => console.log(ship);
+//   }
+
+
+// function selectShips(){
+//   setTimeout(function(){ alert("Select Your Ships!"); }, 2500);
 // }
 
-//next you need a function to build a fleet card
+document.addEventListener('DOMContentLoaded', function() {
+getShips()
+// addToFleet()
 
-// getShips()
+
+//you'll need a method that resets the boards once loaded .
+})
