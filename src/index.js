5cf4aa79db20bc88f6ceeb33d1ccd481d7eda371
Christopher Dent <christopherdent01@gmail.com>
2020-09-16 21:12:38 -0400

    Assign computer fleet to fleet 2

--- src/index.js
+++ src/index.js
@@ -43,22 +43,77 @@ function buildShipCard(ship) {
 function moveShipCard(event){
     event.preventDefault()
     const div = event.target.parentElement
-    div.remove()
+    div.remove()  //removes from avail ships
     const parent = document.getElementById("right")
 
     parent.appendChild(div)
-
+    let btn = div.getElementsByClassName('addBtn')[0]
+    // const btn = document.getElementById(event.target.dataset.id)
+    // btn.setAttribute('data-id', ship.id)
+    // btn.setAttribute('class', 'addBtn' )
+    div.removeChild(btn)
   }
+
+//////////////////////////////////////////////////////////
+
+  function assignCompFleet() {
+      // let configObj = {
+      //     method: "PATCH",
+      //     headers:  {
+      //       "Content-type": "application/json; charset=UTF-8",
+      //       "Accept": "application/json"
+      //     },
+      //     body: JSON.stringify({fleet_id: 2})  ///why won't it assign ships to fleet 2???
+      //   };
+//can you do the fetch to a random number 5 times?
+
+  let configObj = {
+      method: 'PATCH',
+      body: JSON.stringify({ fleet_id: 2 }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+}
+    fetch(`http://localhost:3000/ships/${Math.floor(Math.random() * 34) + 1 }`, configObj)   //here you are changing that particular ships fleet id - but it is not working.  it is assigning 1 instead of 2.
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+        console.log(json)
+    });
+  };
+
+////////////////////  late  night craziness
+
+// function assignCompFleet() {
+//     let configObj = {
+//         method: "POST",
+//         headers:  {
+//           "Content-Type": "application/json",
+//           "Accept": "application/json"
+//         },
+//         // body: JSON.stringify({fleet_id: 3})  ///why won't it assign ships to fleet 2???
+//       };
+// //can you do the fetch to a random number 5 times?
+//
+//   fetch("http://localhost:3000/fleets", configObj)   //here you are changing that particular ships fleet id - but it is not working.  it is assigning 1 instead of 2.
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(json) {
+//       console.log(json)
+//   });
+// };
+//
+
+//////
+
+
 
 function assignFleet(event) {
   event.preventDefault()
-
-        let data = {
-          "fleet_id": 1
-        };
-
       let configObj = {
-          method: "PATCH",  //patch
+          method: "PATCH",
           headers:  {
             "Content-Type": "application/json",
             "Accept": "application/json"
@@ -66,23 +121,26 @@ function assignFleet(event) {
           body: JSON.stringify({fleet_id: 1})
         };
 
-  fetch(`http://localhost:3000/ships/${event.target.dataset.id}`, configObj)
+  fetch(`http://localhost:3000/ships/${event.target.dataset.id}`, configObj)   //here you are changing that particular ships fleet id.
     .then(response => response.json())
     .then(json => {
       if (json.message) {
         alert(json.message)
         battle()
       } else {
-
       }
     })
   }
 
 function battle() {
-  let button = document.createElement("img");
-  button.src = "src/battle.png"
-  document.getElementById('right').appendChild(button)
-
+  let button = document.createElement("button")
+  let div = document.createElement("div")
+  let p = document.createElement("p")
+  div.id = "battlebutton"
+  button.id = "battlebutton"
+  button.innerText = "BATTLE"
+  document.getElementById('right').appendChild(div)
+  div.appendChild(button)
 }
 
  function selectShips(){
@@ -102,7 +160,7 @@ function resetFleet() {
     };
     fetch("http://localhost:3000/fleets", configObj)
     .then(function(response) {
-       response.json();
+      return response.json();
     })
       .then(json => console.log(json));
     }
