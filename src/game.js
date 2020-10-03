687900925a39465c6fe02b33cc09710e97736744
Christopher Dent <christopherdent01@gmail.com>
2020-10-02 22:30:55 -0400

    Begin organizing methods into oo classes

--- src/game.js
+++ src/game.js
@@ -1,16 +1,5 @@
 class Game {
 
-  // constructor()
-
-  // run = () => {
-  //   // this.resetFleet()  //resets everything
-  //   this.getShips() //fetches ship cards & sets up chain of click events
-  //   this.selectShips()  // message to user to select their ships
-  // }
-  //
-
-
-
   selectShips = () => {
    setTimeout(function(){ alert("Select Your Five Ships!"); }, 1000);
   }
@@ -18,44 +7,20 @@ class Game {
   getShips = () => {
   fetch("http://localhost:3000/ships")
     .then(function(response) {
-      return response.json();
+      return response.json();    //why do i HAVE to return it?  Game fails if I don't, but why?
     })
     .then(json => {
       json.forEach(ship => console.log(ship.name));
-      json.forEach(ship => newGame.buildShipCard(ship))
+      json.forEach(ship => this.buildShipCard(ship))
     })
-    .catch(function(error) {  //this stopped working after OO implementation...
+    .catch(function(error) {
       console.log(error.message);
     });
   }
 
    buildShipCard = (ship) => {
-    const card = document.createElement('DIV')
-      card.setAttribute("class", "card")
-      card.setAttribute("data-id", ship.id)
-      const para = document.createElement('P')
-      const para2 = document.createElement('P')
-      const btn = document.createElement("BUTTON")
-      btn.setAttribute('data-id', ship.id)
-      btn.setAttribute('class', 'addBtn' )
-
-      let img = document.createElement("img");
-        img.src = ship.image
-        para.innerText = ship.name
-        para2.innerText = `${ship.country} / ${ship.kind}`
-        para.id = "text"
-
-        img.width = "150";
-        img.height = "150";
-        para.innerText = ship.name
-        btn.innerText = "Add to Fleet"
-        btn.addEventListener("click", newGame.moveShipCard)
-        btn.addEventListener("click", newGame.assignFleet)
-      document.getElementById('main').appendChild(card)
-        card.appendChild(img)
-        card.appendChild(para)
-        card.appendChild(para2)
-        card.appendChild(btn)
+     let shipCard = new ShipCard   ///think about making the methods in the shipCard class static methods.  you don't need instances of them, it's just a wasted line.  but for now do what woks.
+     shipCard.build(ship)
   }
 
   assignFleet = (event) => {
@@ -73,32 +38,21 @@ class Game {
         .then(json => {
           if (json.message) {
             alert(json.message)
-            newGame.removeAddButtons()
-            newGame.assignCompFleet()
-            // newGame.battleButton()
+            this.removeAddButtons()
+            this.assignCompFleet()
+            // this.battleButton()
           } else {
           }
+        })
+        .catch(function(error) {
+          console.log(error.message);
         });
-        // .catch(function(error) {
-        //   console.log(error.message);
-        // });
       }
 
 
-  moveShipCard = (event) => {
-    event.preventDefault()
-    const div = event.target.parentElement
-    div.remove()
-    const parent = document.getElementById("right")
-    parent.appendChild(div)
-    div.classList.remove('card')
-     div.classList.add("cardsmall")
-     // let para = getElementsByClassName("p")//
-    let btn = div.getElementsByClassName('addBtn')[0]
-    div.removeChild(btn)
-
-    let txt = div.innerText
-
+  moveShipCard = (ship) => {
+    let shipCard = new ShipCard   ///think about making the methods in the shipCard class static methods.  you don't need instances of them, it's just a wasted line.  but for now do what woks.
+    shipCard.move(ship)
   }
 
    assignCompFleet = () => {  ///this method assigns a fleet to id 2 via fetch
@@ -115,11 +69,8 @@ class Game {
        fetch('http://localhost:3000/fleets/', configObj)
        .then(function(response) {
          response.json();
-
        })
        .then(function(json){
-
-         console.log(json)
          newGame.battleButton()
          newGame.moveCompFleet()
        })
@@ -144,40 +95,26 @@ class Game {
                  }
 
                 })
-
                 arr.forEach(async function(ship) {
-
                  for (let div of a) {
                    if (ship.id == div.dataset['id']) {
-
                      console.log("it!")
                      div.remove()  //if the ships from that fleet match the dataset ids of the cards on the right, get rid of them.
                      const parent = document.getElementById("right")  //getting ready to render the cards in a fleet
                      parent.appendChild(div)  // append that div to the right side
                      div.classList.remove('card')  //get rid of the old class
-                      div.classList.add("cardsmall")  //add the new
-
+                     div.classList.add("cardsmall")  //add the new
                   } else {
                     console.log("not it!")
                    }
                  }
                })
-
              });
             }
 
-
  battleButton = () => {
-  let button = document.createElement("button")
-  let div = document.createElement("div")
-  let p = document.createElement("p")
-  button.id = "battlebutton"
-  button.classList.add("btn", "btn-danger", "btn-block")
-  button.innerText = "BATTLE"
-  document.getElementById('right').appendChild(div)
-  div.appendChild(p)
-  setTimeout(function(){ div.appendChild(button); }, 1000);
-  button.addEventListener("click", newGame.fight)
+   let shipCard = new ShipCard
+   shipCard.battle()
 }
 
   fight = (event) => {
@@ -188,13 +125,9 @@ class Game {
     .then(json => {
       alert(json.message)
     });
-    let btn = document.createElement("BUTTON")
-    btn.id = 'again'
-    btn.innerText = "Play Again?"
-    let nav = document.getElementById('navbar')
-    // nav.appendChild(btn)
-    setTimeout(function(){ nav.appendChild(btn); }, 2500);
-    btn.addEventListener("click", this.reload)
+    
+    let shipCard = new ShipCard
+    shipCard.playAgain()
   }
 
  removeAddButtons = () => {
@@ -227,11 +160,5 @@ class Game {
     .catch(function(error) {
       console.log(error.message);
     });
-
   }
-
-
-
-
-
 }
--- src/ship.js
+++ src/ship.js
@@ -1,4 +1,69 @@
 class ShipCard {
+//i think most of these should be 'static'
+
+ build = (ship) => {
+    const card = document.createElement('DIV')
+    card.setAttribute("class", "card")
+    card.setAttribute("data-id", ship.id)
+    const para = document.createElement('P')
+    const para2 = document.createElement('P')
+    const btn = document.createElement("BUTTON")
+    btn.setAttribute('data-id', ship.id)
+    btn.setAttribute('class', 'addBtn' )
+    let img = document.createElement("img");
+      img.src = ship.image
+      para.innerText = ship.name
+      para2.innerText = `${ship.country} / ${ship.kind}`
+      para.id = "text"
+      img.width = "150";
+      img.height = "150";
+      para.innerText = ship.name
+      btn.innerText = "Add to Fleet"
+      btn.addEventListener("click", newGame.moveShipCard)
+      btn.addEventListener("click", newGame.assignFleet)
+      document.getElementById('main').appendChild(card)
+      card.appendChild(img)
+      card.appendChild(para)
+      card.appendChild(para2)
+      card.appendChild(btn)
+    }
+
+
+ move = (ship) => {
+const div = event.target.parentElement
+div.remove()
+const parent = document.getElementById("right")
+parent.appendChild(div)
+div.classList.remove('card')
+ div.classList.add("cardsmall")
+ // let para = getElementsByClassName("p")//
+let btn = div.getElementsByClassName('addBtn')[0]
+div.removeChild(btn)
+let txt = div.innerText
+}
+
+  battle = () => {
+    let button = document.createElement("button")
+    let div = document.createElement("div")
+    let p = document.createElement("p")
+    button.id = "battlebutton"
+    button.classList.add("btn", "btn-danger", "btn-block")
+    button.innerText = "BATTLE"
+    document.getElementById('right').appendChild(div)
+    div.appendChild(p)
+    setTimeout(function(){ div.appendChild(button); }, 1000);
+    button.addEventListener("click", newGame.fight)
+  }
+
+  playAgain = () => {
+    let btn = document.createElement("BUTTON")
+    btn.id = 'again'
+    btn.innerText = "Play Again?"
+    let nav = document.getElementById('navbar')
+    setTimeout(function(){ nav.appendChild(btn); }, 2500);
+    btn.addEventListener("click", newGame.reload)
+  }
+
 
 
 }
